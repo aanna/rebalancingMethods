16877dd031068bf6f85ba7e1f0a959dc48dd4557
Kasia <kam.washere@gmail.com>
2016-03-22 16:30:17 +0800

    Remove ignored files

--- rebalancing_offline/Debug/rebalancingOffline.d
+++
@@ -1,7 +0,0 @@
-rebalancingOffline.d: ../rebalancingOffline.cpp \
- /opt/gurobi602/linux64/include/gurobi_c++.h \
- /opt/gurobi602/linux64/include/gurobi_c.h
-
-/opt/gurobi602/linux64/include/gurobi_c++.h:
-
-/opt/gurobi602/linux64/include/gurobi_c.h:
